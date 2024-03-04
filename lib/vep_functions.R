637334cc63e91613cef4ca04293c4628a2ea6c8b
oferisak <doctoroots@gmail.com>
2024-03-04 11:50:45 +0200

    updated vep based annotation for onco51

--- lib/vep_functions.R
+++ lib/vep_functions.R
@@ -5,13 +5,85 @@ vep_hgvs_ext <- "/vep/human/hgvs"
 vep_convert_coordinates<-function(assemb_from,assemb_to,coordinates){
   ext <- glue("/map/human/{assemb_from}/{coordinates}/{assemb_to}?")
   response <- GET(paste(vep_server, ext, sep = ""))
-  stop_for_status(response)
-  message('VEP: Retrieved without errors.')
   char <- rawToChar(response$content)
   res<-jsonlite::fromJSON(char)
+  col_names <- paste0(c("chr_", "start_", "end_"), assemb_to)
+  ret_error <- setNames(data.frame(matrix(NA, nrow = 1, ncol = 3)), col_names)
+  
+  if ('error' %in% names(res)){return(ret_error)}
   return(res$mappings$mapped%>%select(!!sym(glue('chr_{assemb_to}')):=seq_region_name,
                                !!sym(glue('start_{assemb_to}')):=start,
                                      !!sym(glue('end_{assemb_to}')):=end))
+}
+
+find_bad_request<-function(hgvs_notations){
+  for (hn in hgvs_notations){
+    message(hn)
+    annotations<-list('hgvs_notations' = hn,
+                      'refseq'=1,
+                      'per_gene'=1,
+                      'canonical'=1)
+    json_body<-toJSON(annotations)
+    response <- POST(paste(vep_server, vep_hgvs_ext, sep = ""), 
+                     content_type_json(),
+                     accept_json(),
+                     body = json_body)
+    
+    stop_for_status(response)
+    
+  }
+}
+
+vep_cds_to_genomic_coordinates<-function(hgvs_notation,build='grch37'){
+  if (build=='grch37'){
+    server <- "https://grch37.rest.ensembl.org"
+  }else{
+    server<-"https://rest.ensembl.org"
+  }
+  vep_hgvs_ext <- "/vep/human/hgvs/:hgvs_notation"
+  message(glue('Parsing {hgvs_notation} with build {build}'))
+  ext <- glue("/vep/human/hgvs/{hgvs_notation}?refseq=true&canonical=true&ambiguous_hgvs=1&pick=1")
+  
+  r <- GET(paste(server, ext, sep = ""), content_type("application/json"))
+  char <- rawToChar(r$content)
+  
+  res<-jsonlite::fromJSON(char)
+
+  if ('error' %in% names(res)){
+    res<-data.frame(hgvs_notation=hgvs_notation,error=res$error)
+    message(glue('VEP: {res$error}'))
+    return(res)
+  }
+  transcript_cons<-bind_rows(res$transcript_consequences)
+  res<-res%>%
+    select(hgvs_notation=input,chr=seq_region_name,start,end,allele_string,strand,assembly_name)%>%
+    separate(allele_string,into=c('ref','alt'),sep='/')%>%
+    bind_cols(transcript_cons%>%
+                select(-c(gene_symbol_source,biotype,gene_id,impact,strand,variant_allele)))
+    
+  message(glue('VEP: found {nrow(res)} matching variants'))
+  return(res)
+}
+
+vep_basic_annotation<-function(hgvs_notations){
+  message(glue('VEP: submitting {length(hgvs_notations)} variants to VEP annotation..'))
+  all_res<-NULL
+  annotations<-list('hgvs_notations' = hgvs_notations,
+                    'refseq'=1,
+                    'per_gene'=1,
+                    'canonical'=1)
+  json_body<-toJSON(annotations)
+  response <- POST(paste(vep_server, vep_hgvs_ext, sep = ""), 
+                   content_type_json(),
+                   accept_json(),
+                   body = json_body)
+  
+  stop_for_status(response)
+  message('VEP: Retrieved without errors.')
+  char <- rawToChar(response$content)
+  all_res<-jsonlite::fromJSON(char)
+  message(glue('Got response for {nrow(res)}/{length(hgvs_notations)}'))
+  return(all_res)
 }
 
 vep_annotate_variants<-function(hgvs_notations,dbnsfp='REVEL_score,CADD_phred,REVEL_rankscore,VEST4_score,VEST4_rankscore,transcript_match=1'){
